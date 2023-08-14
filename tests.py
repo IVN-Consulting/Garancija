b5aeb628a596a27d9e6ee10f831968f3e10f0a98
Ivan Miljkovic <ivan.miljkovic@toptal.com>
2023-08-14 21:45:42 +0200

    Remove dummy tests

--- tests.py
+++ tests.py
@@ -4,15 +4,3 @@ def setup():
 
 def prvi_test():
     pass
-
-
-def increment(x):
-    return x + 1
-
-
-def test_success():
-    assert increment(3) == 4
-
-
-def test_fail():
-    assert increment(3) == 5
