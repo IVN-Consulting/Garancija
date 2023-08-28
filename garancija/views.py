a8620172ffcc7abb6c340bad89aaeb8a93d6d45b
Buda <nemanja.v992@gmail.com>
2023-08-28 17:01:02 +0200

    Syncer Endpoint

--- garancija/views.py
+++ garancija/views.py
@@ -1,8 +1,26 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from old_code.sync_resources import BackendAPI, FrontendAPI, Syncer
 
 
 class MyFirstEndpoint(APIView):
     def get(self, request):
         return Response("Hello from django rest framework")
 
+class Buda(APIView):
+    def get(self, request):
+        return Response("Hello from Buda")
+
+class SyncerEndpoint(APIView):
+    def get(self,request):
+        group_workspaces = {
+            'User Service': ['Authentication', 'User Management'],
+            'Shop Service': ['Shop API'],
+            'Warranty Service': ['Warranty API']
+        }
+        frontend = FrontendAPI(_type="myhome")
+        backend = BackendAPI(_type="group", workspaces=group_workspaces, _filter=True)
+        syncer = Syncer(backend=backend, frontend_api=frontend)
+        workspaces = syncer.sync()
+
+        return Response(workspaces)
