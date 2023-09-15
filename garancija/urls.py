d25a095c70bc7eaea147608035f4a5dc938b1997
Ivan Miljkovic <ivan.miljkovic@toptal.com>
2023-09-15 21:15:06 +0200

    Fix endpoints

--- garancija/urls.py
+++ garancija/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('health', views.Healthcheck.as_view(), name="healthcheck"),
     path('generics/warranty', views.WarrantyView.as_view(), name="generics-warranty"),
     path('shop', views.ShopListCreateView.as_view(), name="shop-list"),
-    path('shop/<int:pk>/', views.ShopRetrieveView.as_view(), name='shop-retrieve')
+    path('shop/<int:pk>/', views.ShopRetrieveView.as_view(), name='shop-retrieve'),
     path('shop/<int:id>/employees', views.EmployeesByShopView.as_view(), name="shop-employees"),
 ]
