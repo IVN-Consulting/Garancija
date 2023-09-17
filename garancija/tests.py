f28be9d3d27f17a708fadeb7a8bf40275551e539
Buda <nemanja.v992@gmail.com>
2023-09-17 16:58:37 +0200

    Add test for employees by shop view

--- garancija/tests.py
+++ garancija/tests.py
@@ -1,5 +1,5 @@
 import pytest
-from garancija.models import Shop
+from garancija.models import Shop, Employee
 from model_bakery import baker
 from rest_framework.test import APIClient
 from rest_framework.reverse import reverse
@@ -23,3 +23,15 @@ def test_get_existing_shop():
     assert data['id'] == shop.id
     assert data['name'] == shop.name
     assert data['email'] == shop.email
+@pytest.mark.django_db
+def test_get_employees_by_shop():
+    shop = baker.make(Shop)
+    employee = baker.make(Employee, shop=shop, name= 'anyone')
+    employee2 = baker.make(Employee)
+
+    url = reverse("shop-employees", args=[shop.id])
+    response = client.get(url)
+
+    data = response.json()
+    assert employee.id in [x['id'] for x in data]
+    assert not employee2.id in [x['id'] for x in data]
