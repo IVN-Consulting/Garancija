142fc025c0df9dddd73f6c4f3525d5c87da5525c
Buda <nemanja.v992@gmail.com>
2023-09-17 18:42:23 +0200

    Add warranty view test

--- garancija/tests.py
+++ garancija/tests.py
@@ -1,5 +1,5 @@
 import pytest
-from garancija.models import Shop, Employee
+from garancija.models import Shop, Employee, Warranty
 from model_bakery import baker
 from rest_framework.test import APIClient
 from rest_framework.reverse import reverse
@@ -31,6 +31,7 @@ def test_get_employees_by_shop():
 
     url = reverse("shop-employees", args=[shop.id])
     response = client.get(url)
+    assert response.status_code == 200
 
     data = response.json()
     assert employee.id in [x['id'] for x in data]
@@ -42,3 +43,19 @@ def test_get_employees_by_shop():
     data=response.json()
     assert response.status_code == 404
     assert data['detail'] == 'Not found.'
+
+@pytest.mark.django_db
+def test_warranty_view():
+    shop = baker.make(Shop)
+    salesperson = baker.make(Employee, shop=shop)
+    bad_salesperson = baker.make(Employee)
+    warranty = baker.make(Warranty, salesperson=salesperson)
+
+    url = reverse('generics-warranty')
+    response = client.get(url)
+    assert response.status_code == 200
+
+    data= response.json()
+    assert salesperson.id in [x['salesperson']['id'] for x in data]
+    assert shop.id in [x['salesperson']['shop']['id'] for x in data]
+    assert not bad_salesperson.id in [x['salesperson']['id'] for x in data]
