5eee9209f540a9fcf0f8a9af66ffcb7a31a918fb
Ivan Miljkovic <ivan.miljkovic@toptal.com>
2023-10-24 20:42:11 +0200

    Lint

--- user/models.py
+++ user/models.py
@@ -3,7 +3,14 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    shop = models.ForeignKey('garancija.Shop', on_delete=models.DO_NOTHING, related_name="employees", null=True, default=None, blank=True)
+    shop = models.ForeignKey(
+        'garancija.Shop',
+        on_delete=models.DO_NOTHING,
+        related_name="employees",
+        null=True,
+        default=None,
+        blank=True
+    )
     user_type = models.CharField(
         max_length=64,
         choices=(
