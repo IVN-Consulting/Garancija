77c8a4266acbe2d991f739907c66b30f5dab3544
Buda <nemanja.v992@gmail.com>
2023-11-18 19:33:08 +0100

    django-simplejwt

--- garancija/urls.py
+++ garancija/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from garancija import views
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 
@@ -12,4 +16,6 @@ router.register(r'customers', views.CustomersViewSet, basename="customers")
 urlpatterns = [
     *router.urls,
     path('health', views.Healthcheck.as_view(), name="healthcheck"),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
